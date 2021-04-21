f9a103c105362225c37328620e5419b3bfaf2f96
emanuelenistri <emanuele.nistri@gmail.com>
2021-04-21 15:51:18 +0200

    3

--- mysite/src/main.js
+++ mysite/src/main.js
@@ -2,3 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const admin = require('firebase-admin');
+
+const serviceAccount = require('../src/myfiresite-88939-913d2ec95558.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const docRef = db.collection('users').doc('alovelace');
+
+docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
